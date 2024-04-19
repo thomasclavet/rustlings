9ac75a52cd9bbe4b67c4e6f88785ea8d58bde0df
thomas <thomas.clavet@wee-fin.com>
2024-04-19 15:29:38 +0200

    More stuff

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(number_of_apples: i32) -> i32 {
+    let mut price: i32;
+
+    if number_of_apples > 40 {
+        return number_of_apples
+    }
+    number_of_apples * 2
+}
 
 // Don't modify this function!
 #[test]
